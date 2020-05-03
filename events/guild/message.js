bbb137b2c45be51a0d955155cf119db866d8fafe
ayaan1005 <64188607+ayaan1005@users.noreply.github.com>
2020-05-03 13:16:58 +0530

    Add files via upload

---
+++ events/guild/message.js
@@ -0,0 +1,13 @@
+const { prefix } = require("../../botconfig.json")
+
+module.exports = async (bot, message) => {
+    if(message.author.bot || message.channel.type === "dm") return;
+
+    if(!message.content.startsWith(prefix)) return;
+    let args = message.content.slice(prefix.length).trim().split(/ +/g)
+    let cmd = args.shift().toLowerCase()
+
+    let commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd))
+    if(commandfile) commandfile.run(bot, message, args)
+
+}
