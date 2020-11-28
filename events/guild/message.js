160039f678ee475e46fef464a9c3a74a01fa2f7c
AyaanDEV <64188607+ayaan1005@users.noreply.github.com>
2020-11-29 02:31:40 +0530

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -3,8 +3,8 @@ const { prefix, main_ownerid } = require("../../botconfig.json")
 module.exports = async (bot, message) => {
     if(message.author.bot || message.channel.type === "dm") return;
 
-    if(!message.content.startsWith(prefix)) return;
-    let args = message.content.slice(prefix.length).trim().split(/ +/g)
+    if(!message.content.toLowerCase().startsWith(prefix)) return;
+    let args = message.content.slice(prefix.toLowerCase().length).trim().split(/ +/g)
     let cmd = args.shift().toLowerCase()
 
     let commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd))
